da15c0494ca20bf875ea1148ff5b5039dc05732a
JovitaTipon <tiponjovita@gmail.com>
2024-09-25 02:17:47 +0800

    completed task 2(text) under lab 8.1

--- src/Outputs/TextFormat.php
+++ src/Outputs/TextFormat.php
@@ -16,12 +16,30 @@ class TextFormat implements ProfileFormatter
         $output .= "Address: " . implode(", ", $profile->getContactDetails()['address']) . PHP_EOL;
         $output .= "Education: " . $profile->getEducation()['degree'] . " at " . $profile->getEducation()['university'] . PHP_EOL;
         $output .= "Skills: " . implode(", ", $profile->getSkills()) . PHP_EOL;
+        
 
         // Add experience, certifications, etc.
         $output .= "Experience:\n";
         foreach ($profile->getExperience() as $job) {
             $output .= "- " . $job['job_title'] . " at " . $job['company'] . " (" . $job['start_date'] . " to " . $job['end_date'] . ")\n";
         }
+        $output .= "Certifications: \n";
+        foreach ($profile->getCertifications() as $cert) {
+            $output .= $cert['name'] . ", Date Earned: " . $cert['date_earned'] . "\n";
+        }
+        $output .= "Extra-Curricular Activities: \n";
+        foreach ($profile->getExtracurricularActivities() as $extra) {
+            $output .= $extra['role'] . " at " . $extra['organization'] . " (" . $extra['start_date'] . " to " . $extra['end_date'] . ") " . "\n - " . $extra['description'] . "\n";
+        }
+        $output .= "Languages: \n";
+        foreach ($profile->getLanguages() as $lang) {
+            $output .= $lang['language'] . " (" . $lang['proficiency'] . ") \n";
+        }
+        $output .= "References: \n";
+        foreach ($profile->getReferences() as $refe) {
+            $output .= "- " . $refe['name'] . ", " . $refe['position'] . " at " . $refe['company'] . "\nEmail: " . $refe['email'] . "\nPhone Number: " . $refe['phone_number'] . "\n";
+        }
+
         $this->response = $output;
     }
 
